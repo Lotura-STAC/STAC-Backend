af0386b1fe892234fc7aa70b0d612a5c1c559be3
phoenix9469 <phoenix9469@kakao.com>
2023-09-14 22:50:25 +0900

    관리자, 유저 로그인  분리

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post("/login", (req, res) => {
     let id = req.body.id;
     let pw = req.body.pw;
 
-    connection.query(`SELECT id FROM user WHERE id = ? AND pw = ?;`, [id, pw], function (error, results) {
+    connection.query(`SELECT admin_id FROM user WHERE admin_id = ? AND admin_pw = ?;`, [id, pw], function (error, results) {
         if (error) {
             console.log('no matching user blyat');
             console.log(error);
@@ -112,7 +112,22 @@ app.post("/login", (req, res) => {
         }
         //console.log(results);
         if (results.length < 1) {
-            res.status(500).send('비밀번호 오류입니다.')
+            connection.query(`SELECT guest_id FROM user WHERE guest_id = ? AND guest_pw = ?;`, [id, pw], function (error, results) {
+                if (error) {
+                    console.log('no matching user blyat');
+                    console.log(error);
+                    return res.status(500).send('로그인 실패.');
+                }
+                //console.log(results);
+                if (results.length < 1) {
+                    res.status(500).send('비밀번호 오류입니다.')
+                }
+                else {
+                    let accessToken = generateAccessToken(results[0].id);
+                    let refreshToken = generateRefreshToken(results[0].id);
+                    res.json({ accessToken, refreshToken });
+                }
+            });
         }
         else {
             let accessToken = generateAccessToken(results[0].id);
